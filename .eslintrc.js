83127960fc26f2a7ac8b7e4a8bf69e62b2ed3676
Peter Hsu <nadi00913@gmail.com>
2021-07-14 23:55:45 +0800

    eslint-airbnb_with_prrttier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,1 @@
+// put any eslint scenario
